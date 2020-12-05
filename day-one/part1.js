1eb10a7beb9138ddc5994fc62949e1ff577cdc45
jim4067 <jimmyimpulse2@gmail.com>
2020-12-06 00:29:26 +0300

    refactor: moved the code into directories

--- day-one/part1.js
+++ day-one/part1.js
@@ -201,7 +201,6 @@ let arr = [
     1901
 ];
 
-// //the first question 
 
 function num_one(arr) {
     let val1;
@@ -221,32 +220,3 @@ function num_one(arr) {
     const result = val1 * val2;
     return result;
 }
-
-console.log(num_one(arr));
-
-//the second question 
-
-function num_two(arr) {
-    let val1;
-    let val2;
-    let val3;
-    // 
-    let arr_len = arr.length;
-    // 
-    for (let index = 0; index <= arr_len; index++) {
-        for (let in_index = 1; in_index <= arr_len; in_index++) {
-            for (let in_in_index = 2; in_in_index <= arr_len; in_in_index++) {
-                if (arr[index] + arr[in_index] + arr[in_in_index] === 2020) {
-                    val1 = arr[index];
-                    val2 = arr[in_index];
-                    val3 = arr[in_in_index];
-                    break;
-                }
-            }
-        }
-    }
-    const result = val1 * val2 * val3;
-    return result;
-}
-
-console.log(num_two(arr));
