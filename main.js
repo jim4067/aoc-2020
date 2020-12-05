db2715c4d19bab57a3b2bbd8974d04581aef0f03
jim4067 <jimmyimpulse2@gmail.com>
2020-12-05 15:17:01 +0300

    renamed the main file

--- main.js
+++ main.js
@@ -201,23 +201,24 @@ let arr = [
     1901
 ];
 
+// //the first question 
+// function num_one(arr) {
+//     let val1;
+//     let val2;
+//     // 
+//     let arr_len = arr.length;
+//     // 
+//     for (let index = 0; index <= arr_len; index++) {
+//         for (let in_index = 1; in_index <= arr_len; in_index++) {
+//             if (arr[index] + arr[in_index] === 2020) {
+//                 val1 = arr[index];
+//                 val2 = arr[in_index];
+//                 break;
+//             }
+//         }
+//     }
+//     const result = val1 * val2;
+//     return result;
+// }
 
-function num_one(arr) {
-    let val1;
-    let val2;
-    // 
-    let arr_len = arr.length;
-    // 
-    for (let index = 0; index <= arr_len; index++) {
-        for (let in_index = 1; in_index <= arr_len; in_index++) {
-            if (arr[index] + arr[in_index] === 2020) {
-                val1 = arr[index];
-                val2 = arr[in_index];
-                break;
-            }
-        }
-    }
-    const result = val1 * val2;
-    return result;
-}
-console.log(num_one(arr));
+// console.log(num_one(arr));
